e5b4decd1b4892f8314eb4e4da5b7af100569c72
SabadellMattia <34937314+ElSabadell@users.noreply.github.com>
2020-07-17 11:38:32 +0200

    First Upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('.navbar-brand-txt').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('#navbar-logo').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('.white-box').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('.btn-join').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('.nav-link').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('#navbar-logo').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('.navbar-toggler').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$(window).scroll(function(){
+	$('.navbar-brand-mobile').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+
